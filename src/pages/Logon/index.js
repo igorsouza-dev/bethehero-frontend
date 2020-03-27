64a1db3b96a0bf080d86d4b5d4fb6aa0e469c749
Igor Souza <igorsouza.dev@gmail.com>
2020-03-27 17:24:07 -0300

    Added login and profile listing

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 import './styles.css';
 import api from 'services/api';
@@ -10,11 +11,32 @@ import logoImg from 'assets/logo.svg';
 
 export default function Logon() {
   const [id, setId] = useState();
+  const history = useHistory();
   async function handleSubmit(e) {
     e.preventDefault();
-    await api.post('/sessions', {
-      id,
-    });
+    try {
+      const response = await api.post('/sessions', {
+        id,
+      });
+      localStorage.setItem('@BeTheHero', JSON.stringify(response.data));
+      history.push('/profile');
+    } catch (err) {
+      let message = 'Não foi possível fazer login';
+      if (err) {
+        if (err.response) {
+          if (err.response.data) {
+            if (err.response.error) {
+              message = err.response.error;
+            }
+          }
+        }
+      }
+      Swal.fire({
+        title: 'Error!',
+        text: message,
+        icon: 'error',
+      });
+    }
   }
   return (
     <div className="logon-container">
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,40 +1,100 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
-
+import Swal from 'sweetalert2';
 import logoImg from 'assets/logo.svg';
+import api from 'services/api';
+
 import './styles.css';
 
 export default function Profile() {
+  const [name, setName] = useState('');
+  const [ongId, setOngId] = useState();
+  const [incidents, setIncidents] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function getIncidents(id) {
+      try {
+        const response = await api.get('/profile', {
+          headers: {
+            ong_id: id,
+          },
+        });
+        setIncidents(response.data);
+      } catch (e) {
+        Swal.fire({
+          title: 'Error!',
+          text: 'Ocorreu um erro ao buscar os casos',
+          icon: 'error',
+        });
+      }
+    }
+    const ongObj = JSON.parse(localStorage.getItem('@BeTheHero'));
+    if (ongObj) {
+      setName(ongObj.name);
+      setOngId(ongObj.id);
+      getIncidents(ongObj.id);
+    } else {
+      history.push('/');
+    }
+  }, []);
+  function handleLogout() {
+    localStorage.removeItem('@BeTheHero');
+    history.push('/');
+  }
+  async function handleDelete(id) {
+    try {
+      await api.delete(`/incidents/${id}`, {
+        headers: {
+          ong_id: ongId,
+        },
+      });
+      setIncidents(incidents.filter((incident) => incident.id !== id));
+    } catch (e) {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Erro ao deletar caso',
+        icon: 'error',
+      });
+    }
+  }
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Bem vinda, ONG</span>
+        <span>{`Bem vinda, ${name}`}</span>
 
         <Link to="/incidents/new" className="btn">
           Cadastrar novo caso
         </Link>
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
       <h1>Casos cadastrados</h1>
       <ul>
-        <li>
-          <strong>CASO:</strong>
-          <p>caso teste</p>
+        {incidents.map((incident) => (
+          <li key={incident.id}>
+            <strong>CASO:</strong>
+            <p>{incident.title}</p>
 
-          <strong>DESCRIÇÃO:</strong>
-          <p>descrição teste</p>
+            <strong>DESCRIÇÃO:</strong>
+            <p>{incident.description}</p>
 
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
+            <strong>VALOR:</strong>
+            <p>
+              {Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(incident.value)}
+            </p>
 
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
+            <button type="button" onClick={() => handleDelete(incident.id)}>
+              <FiTrash2 size={20} color="#a8a8b3" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
